faceddddd4544e0ffb6182d7dbaccc0228f1192d
Axitvadi <axitvadi@gmail.com>
2023-06-24 18:32:28 +0530

    feat: get woocommerce orders function

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+
+const api = new WooCommerceRestApi({
+    url: process.env.WOOCOMMERCE_URL,
+    consumerKey: process.env.CONSUMERKEY,
+    consumerSecret: process.env.CONSUMERSECRET,
+    version: "wc/v3"
+});
+
+const getOrders = async (pageNumber, limit) => {
+    try {
+        const startDate = new Date('2022-12-12').toISOString();
+        const lastDate = new Date('2022-12-29').toISOString();
+        const perPageLImit = limit ? limit : 5; //limit of order per page
+        const page = pageNumber ? pageNumber : 1; // number of page
+
+        const ordersResponse = await api.get(`orders?after=${startDate}&before=${lastDate}&order=asc&per_page=${perPageLImit}&page=${page}`);
+
+        console.log("orders", ordersResponse.data);
+        console.log("Number of orders", ordersResponse.data.length);
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+getOrders(2, 5); // add a page number and order limit to get a orders of that page
